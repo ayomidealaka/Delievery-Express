8b09277c8deacf34c2d6aad86268dd2926dec017
ayomideAlaka <muizzyomide@gmail.com>
2020-10-02 04:48:27 +0100

    Added functions to handle Tracking ID.

--- src/Components/DeliveryTracker.js
+++ src/Components/DeliveryTracker.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import style from './../App.css';
+import MainPage from './MainPage';
 
-const DeliveryTracker = () => {
-  return (
-    <form>
-      <div className={style.inputdiv}>
-        <input type="text" placeholder="Track your shipment/Tracking ID" />
-        <button>Track my parcel</button>
-      </div>
-    </form>
-  );
-};
+class DeliveryTracker extends Component {
+  constructor(props) {
+    super(props);
 
-export default DeliveryTracker;
+    this.state = {
+      trackingID: '',
+      delivery: '',
+    };
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    let newObj = this.props.compareTrackingId(this.state.trackingID);
+    this.setState({
+      delivery: newObj,
+    });
+
+    console.log(newObj);
+    console.log('the new state is', this.state);
+
+    this.props.history.push('/about');
+  };
+
+  render() {
+    return (
+      <form>
+        <div className={style.inputdiv}>
+          <input
+            name="trackingID"
+            type="text"
+            placeholder="Enter your shipment/Tracking ID"
+            onChange={this.onChangeHandler}
+          />
+          <button onClick={this.onSubmitHandler}>Track my parcel</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default withRouter(DeliveryTracker);
